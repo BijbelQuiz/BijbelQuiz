51b575117002cfe3bc8bc97491f469685258c9a2
ThomasNow Productions <codeportalmail@protonmail.com>
2025-09-15 14:12:15 +0200

    Update

--- app/lib/constants/urls.dart
+++ app/lib/constants/urls.dart
@@ -10,7 +10,24 @@ class AppUrls {
 
   // API endpoints
   static final String emergencyApi = '$baseDomainAPI/emergency.ts';
-  static final String scripturaApiBase = 'https://www.scriptura-api.com/api';
+  static final String scripturaApiBase = 'https://scriptura.bijbelquiz.app/api';
+
+  // Scriptura API Endpoints
+  static final String scripturaRandomApi = '$scripturaApiBase/random';
+  static final String scripturaBooksApi = '$scripturaApiBase/books';
+  static final String scripturaChaptersBaseApi = '$scripturaApiBase/chapters';
+  static final String scripturaVersesBaseApi = '$scripturaApiBase/verses';
+  static final String scripturaSearchBaseApi = '$scripturaApiBase/search';
+  static final String scripturaDaytextBaseApi = '$scripturaApiBase/daytext';
+  static final String scripturaSecureDataApi = '$scripturaApiBase/secure-data';
+  static final String scripturaParseReferencePostApi = '$scripturaApiBase/parse/reference';
+  static final String scripturaParseReferenceGetBaseApi = '$scripturaApiBase/parse/reference';
+  static final String scripturaParseReferencesPostApi = '$scripturaApiBase/parse/references';
+  static final String scripturaVersionsApi = '$scripturaApiBase/versions';
+  static final String scripturaCommentaryBaseApi = '$scripturaApiBase/commentary';
+
+  // Other API Endpoints
+  static final String stripeWebhookApi = '$baseDomainAPI/stripe/webhook';
 
   // App-specific URLs
   static final String donateUrl = '$baseDomain/donate';
