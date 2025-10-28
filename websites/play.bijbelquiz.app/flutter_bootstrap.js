b0eb2703c550e8c1a7fa84558a06dd6e76362c90
ThomasNow Productions <thomasnowprod@proton.me>
2025-10-28 16:23:59 +0100

    Updated version

--- websites/play.bijbelquiz.app/flutter_bootstrap.js
+++ websites/play.bijbelquiz.app/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2619228696"
+    serviceWorkerVersion: "3695184517"
   }
 });
